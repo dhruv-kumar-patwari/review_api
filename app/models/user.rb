8746b9c692a59f6b04438f662a96423f8c991a02
Dhruv Kumar Patwari <dhruv.patwari@zemosolabs.com>
2021-06-23 18:20:47 +0530

    Authentication added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :posts
-  validates :name, presence: true, length: { minimum: 3, maximum: 25 },
-                   uniqueness: { case_sensitive: false }
 
+  validates :name, presence: true,
+                   length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 105 },
+  validates :email, presence: true,
                     uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
                     format: { with: VALID_EMAIL_REGEX }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_22_132123) do
+ActiveRecord::Schema.define(version: 2021_06_23_114747) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2021_06_22_132123) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
